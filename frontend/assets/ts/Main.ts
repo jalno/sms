df9e45aee239e856f180a1ca860d14ae1f1e4ed7
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2017-04-23 12:04:07 +0430

    make ajax send sms

--- frontend/assets/ts/Main.ts
+++ frontend/assets/ts/Main.ts
@@ -2,8 +2,10 @@ import * as $ from "jquery";
 import {SMSList} from "./classes/SMSList";
 import {Gateways} from "./classes/Gateways";
 import {Templates} from "./classes/Templates";
+import Send from "./classes/Send";
 $(function(){
 	SMSList.initIfNeeded();
 	Gateways.initIfNeeded();
 	Templates.initIfNeeded();
+	Send.initIfNeeded();
 });
