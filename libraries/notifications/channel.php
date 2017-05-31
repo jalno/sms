e8c39f0a171785d13b6649021c46419e3139d2d4
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2017-05-31 15:11:38 +0430

    ignore notification if there is no relevant template

--- libraries/notifications/channel.php
+++ libraries/notifications/channel.php
@@ -1,16 +1,24 @@
 <?php
 namespace packages\sms\notifications;
 use \packages\base\event;
+use \packages\base\translator;
 use \packages\notifications;
 use \packages\sms\api;
+use \packages\sms\template;
 class channel extends notifications\channel{
 	public function notify(event $event){
-		foreach($event->getTargetUsers() as $user){
-			$api = new api();
-			$arguments = array_replace(array('user' => $user), $event->getArguments());
-			$api->template($event->getName(), $arguments);
-			$api->to($user->cellphone, $user);
-			$api->send();
+		$lang = translator::getShortCodeLang();
+		$template = new template();
+		$template->where('name', $event->getName());
+		$template->where('lang', $lang);
+		if($template->has()){
+			foreach($event->getTargetUsers() as $user){
+				$api = new api();
+				$arguments = array_replace(array('user' => $user), $event->getArguments());
+				$api->template($event->getName(), $arguments);
+				$api->to($user->cellphone, $user);
+				$api->send();
+			}
 		}
 	}
 }
