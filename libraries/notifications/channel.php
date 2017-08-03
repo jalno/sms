747bb42835207a1b22c2577af9d8a03be5ff28ec
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2017-08-03 17:31:10 +0430

    catch errors when there is no active number

--- libraries/notifications/channel.php
+++ libraries/notifications/channel.php
@@ -5,6 +5,8 @@ use \packages\base\translator;
 use \packages\notifications;
 use \packages\sms\api;
 use \packages\sms\template;
+use \packages\sms\deactivedNumberException;
+use \packages\sms\defaultNumberException;
 class channel extends notifications\channel{
 	public function notify(event $event){
 		$lang = translator::getShortCodeLang();
@@ -12,12 +14,18 @@ class channel extends notifications\channel{
 		$template->where('name', $event->getName());
 		$template->where('lang', $lang);
 		if($template->has()){
-			foreach($event->getTargetUsers() as $user){
-				$api = new api();
-				$arguments = array_replace(array('user' => $user), $event->getArguments());
-				$api->template($event->getName(), $arguments);
-				$api->to($user->cellphone, $user);
-				$api->send();
+			try{
+				foreach($event->getTargetUsers() as $user){
+					$api = new api();
+					$arguments = array_replace(array('user' => $user), $event->getArguments());
+					$api->template($event->getName(), $arguments);
+					$api->to($user->cellphone, $user);
+					$api->send();
+				}
+			}catch(deactivedNumberException $e){
+
+			}catch(defaultNumberException $e){
+				
 			}
 		}
 	}
